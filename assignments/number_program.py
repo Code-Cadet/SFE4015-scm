7353d85fd1ebc073cecaf3ebce953538ee4790fb
aisha Kulane <aishakulane01@gmail.com>
2025-06-03 21:48:16 +0300

    New changes by adding the sum functions

--- assignments/number_program.py
+++ assignments/number_program.py
@@ -1,3 +1,14 @@
+# calculates the sum of numbers from 0 to 10
+#New changes for merging
+# The range(0, 11) generates numbers from 0 up to but not including 11
+numbers = range(0, 11)
+
+# Use the sum function to add all numbers in the range
+total_sum = sum(numbers)
+
+# Print the result
+print("The sum of numbers from 0 to 10 is:", total_sum)
+
 """
 Function to compute even numbers and their sum from a given list of numbers.
 """
--- assignments/sum_integers.py
+++ assignments/sum_integers.py
@@ -1,5 +1,5 @@
 # calculates the sum of numbers from 0 to 10
-#New changes for merging
+
 # The range(0, 11) generates numbers from 0 up to but not including 11
 numbers = range(0, 11)
 
